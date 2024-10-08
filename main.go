17cb8a6ee36cd7058a9522bfc205a9890d7dbe27
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2024-10-08 04:28:52 +0100

    updates

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 type Todo struct {
-	Id        int `json:"id"`
+	Id        int `json:"id" bson:"_id"`
 	Completed bool   `json:"completed"`
 	Body      string `json:"body"`
 }
 
+var collection *mongo.Collection
+
 func main() {
-	
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading env file", err)	
+	}
+
+	mongoUri := os.Getenv("MONGODB_URI")
+	clientOptions := options.Client().ApplyURI(mongoUri)
+	client, err := mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)	
+	}
+	err := client.Ping(context.Background(), nil)
 }
